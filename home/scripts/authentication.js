266e98b8d174b2b941e32d15f68ff671bf9cab42
Tyler Childs <email@tychi.me>
2023-01-10 12:00:02 -0800

    os to js pipe

--- home/scripts/authentication.js
+++ home/scripts/authentication.js
@@ -1,6 +1,16 @@
 import module from '/system/module.js'
 import $controlBox from '/scripts/control-box.js'
 
+      // access the pre-bundled global API functions
+      const { invoke } = window.__TAURI__.tauri
+
+      // now we can call our Command!
+      // Right-click the application background and open the developer tools.
+      // You will see "Hello, World!" printed in the console!
+      invoke('greet', { name: 'World' })
+        // `invoke` returns a Promise
+        .then((response) => console.log(response))
+
 const $ = module('authentication', {
   filter: ''
 })
