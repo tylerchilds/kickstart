b0a2f9e3cefcf5207cea7ab9b001d8bd67902aad
Tyler Childs <email@tychi.me>
2023-01-14 09:01:00 -0800

    initial plumbing of calling an external binary

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,49 +1,56 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+	all(not(debug_assertions), target_os = "windows"),
+	windows_subsystem = "windows"
 )]
 
-use tauri::Manager;
+use std::error::Error;
+use tauri::{
+	api::process::{Command, CommandEvent},
+	Manager,
+};
 use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 use tracing::info;
 use tracing_subscriber;
 use serde_json::json;
-use gilrs::{Gilrs, Event, Axis, Button, EventType};
+use gilrs::{Gilrs, Event, EventType};
 
 struct AsyncProcInputTx {
-    inner: Mutex<mpsc::Sender<String>>,
+	inner: Mutex<mpsc::Sender<String>>,
 }
 
 fn main() {
-    tracing_subscriber::fmt::init();
+	tracing_subscriber::fmt::init();
 
-    let (async_proc_input_tx, async_proc_input_rx) = mpsc::channel(1);
-    let (async_proc_output_tx, mut async_proc_output_rx) = mpsc::channel(1);
+	let (async_proc_input_tx, async_proc_input_rx) = mpsc::channel(1);
+	let (async_proc_output_tx, mut async_proc_output_rx) = mpsc::channel(1);
 
-    tauri::Builder::default()
-        .manage(AsyncProcInputTx {
-            inner: Mutex::new(async_proc_input_tx),
-        })
-        .invoke_handler(tauri::generate_handler![js2rs])
-        .setup(|app| {
-            tauri::async_runtime::spawn(async move {
-                async_process_model(
-                    async_proc_input_rx,
-                    async_proc_output_tx,
-                ).await
-            });
+	tauri::Builder::default()
+		.manage(AsyncProcInputTx {
+			inner: Mutex::new(async_proc_input_tx),
+		})
+		.invoke_handler(tauri::generate_handler![js2rs])
+		.setup(|app| {
+			/* two way channel from rust async*/
+			tauri::async_runtime::spawn(async move {
+				async_process_model(
+					async_proc_input_rx,
+					async_proc_output_tx,
+					).await
+			});
 
-            let app_handle = app.handle();
-            tauri::async_runtime::spawn(async move {
-                loop {
-                    if let Some(output) = async_proc_output_rx.recv().await {
-                        rs2js(output, &app_handle);
-                    }
-                }
-            });
+			/* one way channel from rust to javascript */
+			let app_handle = app.handle();
+			tauri::async_runtime::spawn(async move {
+				loop {
+					if let Some(output) = async_proc_output_rx.recv().await {
+						rs2js(output, &app_handle);
+					}
+				}
+			});
 
-            let app_handle = app.handle();
+			/* pipe system level gamepad data from rust to javasccript */
+			let app_handle = app.handle();
 			tauri::async_runtime::spawn(async move {
 				let mut gilrs = Gilrs::new().unwrap();
 
@@ -54,7 +61,7 @@ fn main() {
 
 				loop {
 					// Examine new events
-					while let Some(Event { id, event, time }) = gilrs.next_event() {
+					while let Some(Event { id, event, time: _time }) = gilrs.next_event() {
 						match event {
 							EventType::AxisChanged(axis, value, _code) => {
 								let message = json!({
@@ -86,42 +93,62 @@ fn main() {
 
 					}
 				}
-            });
+			});
 
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+			/* get sillyz.computer binary to run*/
+			let window = app.get_window("main").unwrap();
+			tauri::async_runtime::spawn(async move {
+				let (mut rx, mut child) = Command::new_sidecar("sillyz.computer")
+					.expect("failed to setup `sillyz.computer` sidecar")
+					.spawn()
+					.expect("Failed to spawn packaged node");
+
+				while let Some(event) = rx.recv().await {
+					if let CommandEvent::Stdout(line) = event {
+						window
+							.emit("message", Some(format!("'{}'", line)))
+							.expect("failed to emit event");
+
+					    // write to stdin
+					    child.write("message from Rust\n".as_bytes()).unwrap();
+					}
+				}
+			});
+
+			Ok(())
+		})
+	.run(tauri::generate_context!())
+	.expect("error while running tauri application");
 }
 
 fn rs2js<R: tauri::Runtime>(message: String, manager: &impl Manager<R>) {
-    info!(?message, "rs2js");
-    manager
-        .emit_all("rs2js", format!("{}", message))
-        .unwrap();
+	info!(?message, "rs2js");
+	manager
+		.emit_all("rs2js", format!("{}", message))
+		.unwrap();
 }
 
 #[tauri::command]
 async fn js2rs(
-    message: String,
-    state: tauri::State<'_, AsyncProcInputTx>,
+	message: String,
+	state: tauri::State<'_, AsyncProcInputTx>,
 ) -> Result<(), String> {
-    info!(?message, "js2rs");
-    let async_proc_input_tx = state.inner.lock().await;
-    async_proc_input_tx
-        .send(message)
-        .await
-        .map_err(|e| e.to_string())
+	info!(?message, "js2rs");
+	let async_proc_input_tx = state.inner.lock().await;
+	async_proc_input_tx
+		.send(message)
+		.await
+		.map_err(|e| e.to_string())
 }
 
 async fn async_process_model(
-    mut input_rx: mpsc::Receiver<String>,
-    output_tx: mpsc::Sender<String>,
+	mut input_rx: mpsc::Receiver<String>,
+	output_tx: mpsc::Sender<String>,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    while let Some(input) = input_rx.recv().await {
-        let output = input;
-        output_tx.send(output).await?;
-    }
+	while let Some(input) = input_rx.recv().await {
+		let output = input;
+		output_tx.send(output).await?;
+	}
 
-    Ok(())
+	Ok(())
 }
