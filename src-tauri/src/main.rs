dee03b2f9b684fa857641c17fd675e8cea26e113
Tyler Childs <email@tychi.me>
2023-01-26 22:53:35 -0800

    bare minimum for merge

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,6 @@
 )]
 
 use tauri::{
-	api::process::{Command, CommandEvent},
 	Manager,
 };
 use tokio::sync::mpsc;
@@ -94,27 +93,6 @@ fn main() {
 				}
 			});
 
-			/* get sillyz.computer binary to run*/
-            /*
-			let window = app.get_window("main").unwrap();
-			tauri::async_runtime::spawn(async move {
-				let (mut rx, mut child) = Command::new_sidecar("sillyz.computer")
-					.expect("failed to setup `sillyz.computer` sidecar")
-					.spawn()
-					.expect("Failed to spawn packaged node");
-
-				while let Some(event) = rx.recv().await {
-					if let CommandEvent::Stdout(line) = event {
-						window
-							.emit("message", Some(format!("'{}'", line)))
-							.expect("failed to emit event");
-
-					    // write to stdin
-					    child.write("message from Rust\n".as_bytes()).unwrap();
-					}
-				}
-			});
-*/
 			Ok(())
 		})
 	.run(tauri::generate_context!())
