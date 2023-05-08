2142c7b4d48407436f668fac709a66222c0c9e5c
Tyler Childs <email@tychi.me>
2023-05-08 16:45:57 -0700

    run pup in app

--- mod.js
+++ mod.js
@@ -1,31 +1,36 @@
-import { mergeReadableStreams } from "https://deno.land/std@0.186.0/streams/merge_readable_streams.ts";
-import { ensureDir } from "https://deno.land/std@0.144.0/fs/ensure_dir.ts";
+import { Pup } from "https://deno.land/x/pup/mod.ts"
 
-ensureDir(`${Deno.cwd()}/tmp/logs`)
-const log = `${Deno.cwd()}/tmp/logs/${new Date().toISOString()}.txt`
+const configuration = {
+  "processes": [
+    {
+      "id": "sillonious-tag",
+      "cmd": ["deno", "task", "start-tag"],
+      "autostart": true
+    },
+    {
+      "id": "sillonious-solid",
+      "cmd": ["deno", "task", "start-solid"],
+      "autostart": true
+    },
+    {
+      "id": "sillonious-braid",
+      "cmd": ["deno", "task", "start-braid"],
+      "autostart": true
+    },
+    {
+      "id": "sillonious-gun",
+      "cmd": ["deno", "task", "start-gun"],
+      "autostart": true
+    },
+    {
+      "id": "sillonious-edge",
+      "cmd": ["deno", "task", "start-edge"],
+      "autostart": true
+    }
+  ]
+}
 
-console.log('a log of this sillonious session is available in:\n', log)
-const file = await Deno.open(log, {
-  read: true,
-  write: true,
-  create: true,
-});
+const pup = await new Pup(configuration /* OPTIONAL: , statusFile */)
 
-const command = new Deno.Command(Deno.execPath(), {
-	stdout: "piped",
-  stderr: "piped",
-  args: [
-    "task",
-    "start",
-  ],
-});
-
-// create subprocess and collect output
-const process = await command.spawn();
-
-const joined = mergeReadableStreams(
-  process.stdout,
-  process.stderr,
-);
-
-joined.pipeTo(file.writable).then(() => console.log("pipe join done"));
+// Go!
+pup.init()
