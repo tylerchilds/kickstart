cedc39cc680a8fdaad6512753202c212bd417932
Tyler Childs <email@tychi.me>
2023-05-19 23:37:41 -0700

    feature flag config

--- mod.js
+++ mod.js
@@ -1,36 +1,51 @@
 import { Pup } from "https://deno.land/x/pup/mod.ts"
 
-const configuration = {
-  "processes": [
-    {
+const processConfiguration = {
+  notorious: 'sillonious',
+  tag: true,
+  solid: false,
+  braid: false,
+  gun: true,
+  edge: true,
+  features: {
+    tag: {
       "id": "sillonious-tag",
       "cmd": ["deno", "task", "start-tag"],
       "autostart": true
     },
-    {
+    solid: {
       "id": "sillonious-solid",
       "cmd": ["deno", "task", "start-solid"],
       "autostart": true
     },
-    {
+    braid: {
       "id": "sillonious-braid",
       "cmd": ["deno", "task", "start-braid"],
       "autostart": true
     },
-    {
+    gun: {
       "id": "sillonious-gun",
       "cmd": ["deno", "task", "start-gun"],
       "autostart": true
     },
-    {
+    edge: {
       "id": "sillonious-edge",
       "cmd": ["deno", "task", "start-edge"],
       "autostart": true
     }
-  ]
+  }
 }
 
-const pup = await new Pup(configuration /* OPTIONAL: , statusFile */)
+
+const activeFeatures = Object.keys(processConfiguration)
+  .filter(x => processConfiguration[x] === true)
+  .map(x => processConfiguration.features[x])
+
+console.log(activeFeatures)
+
+const pup = await new Pup({
+  "processes": activeFeatures
+})
 
 // Go!
 pup.init()
