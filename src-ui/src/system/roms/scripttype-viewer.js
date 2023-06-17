7c954cbc33ee77845f1a70827a1480c1e61caeb9
Tyler Childs <email@tychi.me>
2023-06-17 07:16:05 -0700

    the rest of the elephant

--- src-ui/src/system/roms/scripttype-viewer.js
+++ src-ui/src/system/roms/scripttype-viewer.js
@@ -1,5 +1,6 @@
 import module from '../module.js'
 import database from '../database.js'
+import './hyper-piano.js'
 
 const $ = module('scripttype-viewer')
 const sourceLocation = '/scripts/' + window.location.pathname.split('/view/')[1]
